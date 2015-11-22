715185ed873373c68693452f2c4c398d5b3deba0
aditi1795 <aditib2009@gmail.com>
2015-11-22 13:01:26 +0530

    Create mid1.php

---
+++ mid1.php
@@ -0,0 +1,8 @@
+<div class="grid_1">
+		<div class="container">
+		  <div class="box_1  wow fadeInUpBig" data-wow-delay="0.4s">
+			<h3>How to search names..</h3>
+			<p>
+			 just select the year , gender , apply filters and click on submit button to get results in seconds.
+			</p>
+		  </div>
