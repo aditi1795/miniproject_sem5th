af3539da1e70f58b7c43f427f09148631d15cce1
aditi1795 <aditib2009@gmail.com>
2015-11-24 20:20:57 +0530

    Create top1.php

---
+++ top1.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Welcome to the world of baby names</title>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery-1.11.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<!-- Custom Theme files -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--webfont-->
+<link href='http://fonts.googleapis.com/css?family=Lato:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
+<!----font-Awesome----->
+<link rel="stylesheet" href="fonts/css/font-awesome.min.css">
+<!----font-Awesome----->
+<!--Animation-->
+<script src="js/wow.min.js"></script>
+<link href="css/animate.css" rel='stylesheet' type='text/css' />
+<script>
+	new WOW().init();
+</script>
+
+</head>
+<body>
+<div class="header wow fadeInUpBig" data-wow-delay="0.4s">
+   <div class="container">
+	  <div class="header_top">
+		<h1 style="color:gray;">Welcome to </h1>
+		<h2 style="color:brown;">Baby names search</h2>
+	  </div>
+   </div>
+ </div>
